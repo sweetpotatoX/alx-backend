810af8d6cbb4bba85b6c12033fa2a71aecc792ae
sweetpotatoX <abdobahaa53@gmail.com>
2024-09-10 19:00:37 +0300

    again

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,41 +1,37 @@
 #!/usr/bin/env python3
 """A Basic Flask app.
 """
-from flask import Flask, render_template
 from flask_babel import Babel
-
-babel = Babel()
+from flask import Flask, render_template, request
 
 
 class Config:
-    """Config class for the Flask app.
+    """Represents a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
-    babel.init_app(app)
 
-    @babel.localeselector
-    def get_locale():
-        """Retrieves the locale for a web page.
-"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
-    @app.route('/')
-    def index():
-        """Return the index page.
-        """
-        return render_template('2-index.html')
 
-    return app
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
