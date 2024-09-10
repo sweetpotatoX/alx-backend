a9ee6864e8d8996f3369454487fc93c3a3a166dc
sweetpotatoX <abdobahaa53@gmail.com>
2024-09-10 18:43:51 +0300

    two

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+# Instantiate the Babel object
+babel = Babel()
+
+
+class Config:
+    """Config class for the Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize Babel with the Flask app
+    babel.init_app(app)
+
+    @app.route('/')
+    def index():
+        """Return the index page.
+        """
+        return render_template('1-index.html')
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
