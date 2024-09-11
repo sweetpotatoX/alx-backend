5655572ef9170353fedf691bef0bb28ca52ff993
sweetpotatoX <abdobahaa53@gmail.com>
2024-09-11 13:00:31 +0300

    test 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,7 +2,8 @@
 """A Basic Flask app with internationalization support.
 """
 from flask_babel import Babel
-from flask import Flask, render_template, request
+from typing import Union, Dict
+from flask import Flask, render_template, request, g
 
 
 class Config:
@@ -17,6 +18,29 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Performs some routines before each request's resolution.
+    """
+    user = get_user()
+    g.user = user
 
 
 @babel.localeselector
@@ -38,7 +62,7 @@ def get_locale() -> str:
 def get_index() -> str:
     """The home/index page.
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
