26e4e9103a8d76aa42579cb670048ac05cec8279
sweetpotatoX <abdobahaa53@gmail.com>
2024-12-08 06:15:20 +0200

    test

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""Basic flask app start.
+"""A Basic Flask app.
 """
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
-    """The home/indexpage.
+def get_index() -> str:
+    """The home/index page.
     """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
